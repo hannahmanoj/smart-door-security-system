507cc1a2324fc40d88a0b159d0e003648926acb4
Hannah <145972702+hannahmanoj@users.noreply.github.com>
2025-03-03 22:09:01 +0000

    Delete app directory

--- app/SignUp.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function SignUp() {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Sign Up Screen</Text>
-        </View>
-    );
-}
