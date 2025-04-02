556afb77efd30712cebda32aa11202b029f5e970
Hannah <145972702+hannahmanoj@users.noreply.github.com>
2025-04-02 23:59:07 +0100

    Add files via upload

---
+++ Facial Recognition/final_hardware_code.py
@@ -0,0 +1,238 @@
+import face_recognition
+import cv2
+import numpy as np
+from picamera2 import Picamera2
+import time
+from time import sleep
+import pickle
+from gpiozero import LED, MotionSensor
+import sqlite3
+from datetime import datetime
+import os
+import firebase_admin
+from firebase_admin import credentials, firestore, storage
+import requests
+import hashlib
+from pyfingerprint.pyfingerprint import PyFingerprint
+from pyfingerprint.pyfingerprint import FINGERPRINT_CHARBUFFER1
+import RPi.GPIO as GPIO
+
+# Initialize GPIO for LED (fingerprint)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+#GPIO.setup(18, GPIO.IN)
+
+# Firebase setup
+cred = credentials.Certificate("/home/Group22/Desktop/FaceRecognition/smart-security-system-417f0-firebase-adminsdk-fbsvc-ca47f4df54.json")
+firebase_admin.initialize_app(cred, {"storageBucket": "smart-security-system-417f0.appspot.com"})
+db = firestore.client()
+bucket = storage.bucket()
+
+
+# Motion sensor and LED
+green_led = LED(17)
+pir = MotionSensor(4)
+green_led.off()
+
+# Load face encodings
+print("[INFO] loading encodings...")
+with open("encodings.pickle", "rb") as f:
+    data = pickle.loads(f.read())
+known_face_encodings = data["encodings"]
+known_face_names = data["names"]
+
+# Initialize camera
+picam2 = Picamera2()
+picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (1280, 720)}))
+picam2.start()
+
+cv_scaler = 4  # Scale factor for performance
+face_locations = []
+face_encodings = []
+face_names = []
+frame_count = 0
+start_time = time.time()
+fps = 0
+
+prev_detected_names = set()
+
+# Dictionary to track last insertion time per person
+last_insert_times = {}
+
+# Fingerprint sensor initialization
+try:
+    f = PyFingerprint('/dev/ttyS0', 57600, 0xFFFFFFFF, 0x00000000)
+    if f.verifyPassword() == False:
+        raise ValueError('The given fingerprint sensor password is wrong!')
+except Exception as e:
+    print('The fingerprint sensor could not be initialized!')
+    print('Exception message: ' + str(e))
+    exit(1)
+
+def image_to_blob(image):
+    """Convert image (numpy array) to binary format (BLOB)"""
+    _, buffer = cv2.imencode('.jpg', image)
+    return buffer.tobytes()
+
+def save_to_database(name, frame):
+    global last_insert_times
+    
+    current_time = datetime.now()
+    
+    #check if this person has bee logged in the last five minutes
+    if name in last_insert_times and (current_time - last_insert_times[name]).total_seconds() < 300:
+        print(f"Skipping insertion for {name}. Less than 5 minutes since last save. ")
+        return
+    
+    #update the last insert time for this person
+    last_insert_times[name] = current_time
+    
+    #connect to the sqlite database (faces.db)
+    conn = sqlite3.connect("faces_log.db")
+    cursor = conn.cursor()
+    
+    #generate a timestamp for database entry
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    #define folder where images will be saved
+    folder = "logs"
+    
+    #checks if folder exists if not it will create it
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+        
+    # Convert the image to BLOB format
+    image_blob = image_to_blob(frame)
+    
+    
+    # Insert a record into the "face_logs" table with name, timestamp, and image path
+    cursor.execute("INSERT INTO face_logs (name, timestamp, image) VALUES (?, ?, ?)", 
+                   (name, timestamp, image_blob))
+    
+    # Commit the transaction to save changes in the database
+    conn.commit()
+    
+    # Close the database connection
+    conn.close()
+    
+    print(f"Saved {name} to database at {timestamp}")
+    
+
+
+# Function to capture and process fingerprint
+def process_fingerprint():
+    try:
+        print('Waiting for fingerprint...')
+        while (f.readImage() == False):
+            pass
+        
+        f.convertImage(FINGERPRINT_CHARBUFFER1)
+        result = f.searchTemplate()
+        positionNumber = result[0]
+        accuracyScore = result[1]
+
+        if positionNumber == -1:
+            print('Fingerprint not recognized. Denied.')
+            GPIO.setup(18, GPIO.IN) 
+            return "Denied"
+        else:
+            print(f'Fingerprint recognized. Accepted. Position #{positionNumber}, Accuracy Score: {accuracyScore}')
+            GPIO.setup(18, GPIO.OUT)
+            print("door unclocked")
+            sleep(5)
+            GPIO.setup(18, GPIO.IN)
+            print("door locked")
+            return "Accepted"
+    
+    except Exception as e:
+        print('Fingerprint operation failed!')
+        print('Exception message: ' + str(e))
+        return "Denied"
+
+
+# Process frame from camera
+def process_frame(frame):
+    global face_locations, face_encodings, face_names, prev_detected_names
+    resized_frame = cv2.resize(frame, (0, 0), fx=(1/4), fy=(1/4))
+    rgb_resized_frame = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2RGB)
+    face_locations = face_recognition.face_locations(rgb_resized_frame)
+    face_encodings = face_recognition.face_encodings(rgb_resized_frame, face_locations, model='large')
+    
+    face_names = []
+    for face_encoding in face_encodings:
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        name = "Unknown"
+        
+        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = known_face_names[best_match_index]
+        
+        face_names.append(name)
+        
+        #save recognised/unrecognised face to database
+        save_to_database(name, frame)
+        
+    detected_set = set(face_names)
+        
+    if detected_set != prev_detected_names:
+        print(f"Detected: {', '.join(detected_set)}")
+        prev_detected_names = detected_set
+        
+        
+    
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        if name != "Unknown":
+            print(f"Face recognized: {name}")
+            picam2.stop()  # Stop the camera
+            result = process_fingerprint()  # Call fingerprint scan after face recognition
+            print(f"Fingerprint scan result: {result}")
+            return frame
+    
+    return frame
+
+def draw_results(frame):
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        top *= cv_scaler
+        right *= cv_scaler
+        bottom *= cv_scaler
+        left *= cv_scaler
+        cv2.rectangle(frame, (left, top), (right, bottom), (244, 42, 3), 3)
+        cv2.rectangle(frame, (left - 3, top - 35), (right + 3, top), (244, 42, 3), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, top - 6), font, 1.0, (255, 255, 255), 1)
+    return frame
+
+def calculate_fps():
+    global frame_count, start_time, fps
+    frame_count += 1
+    elapsed_time = time.time() - start_time
+    if elapsed_time > 1:
+        fps = frame_count / elapsed_time
+        frame_count = 0
+        start_time = time.time()
+    return fps
+
+# Main loop
+while True:
+    pir.wait_for_motion()
+    print("Motion detected")
+    green_led.on()
+    
+    while pir.motion_detected:
+        frame = picam2.capture_array()
+        processed_frame = process_frame(frame)
+        display_frame = draw_results(processed_frame)
+        current_fps = calculate_fps()
+        cv2.putText(display_frame, f"FPS: {current_fps:.1f}", (display_frame.shape[1] - 150, 30), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv2.imshow('Video', display_frame)
+        if cv2.waitKey(1) == ord("q"):
+            break
+    
+    green_led.off()
+    print("Motion stopped")
+    cv2.destroyAllWindows()
+
+cv2.destroyAllWindows()
+picam2.stop()
